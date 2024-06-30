e749bef99994a47321c9d37c4bf977dc4587a14e
tlecoz <fanthomas.lecoz@gmail.com>
2024-06-30 16:31:34 +0200

    Guide Sample ok

--- src/samples/Guide/Guide_Sample.ts
+++ src/samples/Guide/Guide_Sample.ts
@@ -1,4 +1,4 @@
-import { Align, Display2D, Path, RenderStack, SolidColor, SolidFill, Stage2D } from "xcanvas-ts";
+import { Align, CirclePath, Display2D, RenderStack, SolidColor, SolidFill, Stage2D } from "xcanvas-ts";
 import { Sample } from "../Align/Sample";
 import { ChristmasTreePath } from "../GetBounds/ChristmasTreePath";
 
@@ -8,19 +8,38 @@ export class Guide_Sample extends Sample {
     protected async start(stage: Stage2D) {
 
 
-        const shape = stage.appendChild(new Display2D(500, 500, new RenderStack([
+        const shapeSize = 500;
+
+
+
+
+        const shape = stage.appendChild(new Display2D(shapeSize, shapeSize, new RenderStack([
             ChristmasTreePath.instance,
             new SolidFill(new SolidColor(0xffcc00))
         ])))
+
+        const interactivePath = ChristmasTreePath.instance.getInteractivePath(shape);
 
         shape.x = stage.stageWidth * 0.5;
         shape.y = stage.stageHeight * 0.5;
         shape.align(Align.CENTER);
 
+        const circle = stage.appendChild(new Display2D(20, 20, new RenderStack([
+            CirclePath.instance,
+            new SolidFill(new SolidColor(0xff0000)),
+        ])))
+        circle.align(Align.CENTER);
 
-        const path: Path = shape.get((val) => val.isPath)[0];
+        const startTime = new Date().getTime();
+        const durationInMilliseconds = 5000;
+        stage.addEventListener(Stage2D.DRAW_BEGIN, () => {
+            const time = new Date().getTime() - startTime;
+            const { x, y } = interactivePath.getPointAtTime(shape, time, durationInMilliseconds);
+            circle.x = x;
+            circle.y = y;
+        })
 
-        path
+
     }
 
 
